5619f8c031691a0bb6c933e7e8b25d84205236a3
anamelotm <analuisa.tomita@hotmail.com>
2021-07-01 15:16:54 -0300

    Projeto final JS avançado criado

---
+++ app.js
@@ -0,0 +1,39 @@
+const readline = require ('readline');
+
+const livros = require ('./database');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function input(prompt) {
+    console.log(prompt);
+    return (await r1[Symbol.asyncIterator]().next()).value;
+}
+
+async function main(){
+
+    const entradaInicial = await input('Deseja buscar um livro? S/N ');
+
+    if (entradaInicial.toLocaleUpperCase() === 'S') {
+        console.log('Essas são as categorias disponíveis: ');
+        console.log('Produtividade e estilo de vida', '/História brasileira', '/Américas', '/Tecnologia', '/Estilo de vida');
+        
+        const entradaCategoria = await input('Qual categoria você escolhe? ');
+
+        const retorno = livros.filter(livro => livro.categoria === entradaCategoria);
+        console.table(retorno);
+    }
+    else {
+        const livrosOrdenados = livros.sort((a,b) => a.paginas - b.paginas);
+        console.log('Esses são os livros disponíveis:');
+        console.table(livrosOrdenados);
+    }
+
+    r1.close();
+}
+
+main();
+
+
